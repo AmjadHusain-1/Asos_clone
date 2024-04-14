6d066e191813a7ae1655fbe91c045483e7adafff
Amjad16032000 <136847440+Amjad16032000@users.noreply.github.com>
2024-04-14 23:25:53 +0530

    final product

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Asos_clone
+# ASOS
+
+ASOS is a British online fashion and cosmetic retailer. The company was founded in 2000 in London, primarily aimed at young adults.
+
+# ASOS CLone
+
+This is the clone of ASOS which we i have done as a part of our project at Prepleaf. Its functional clone of the website. 
+
+## Technologies used
+-HTML
+- Javascript
+- CSS
+- React
+- Json Data
+
+## Functional things
+
+- User signup page 
+- login page with form validation and data managed with  json server
+- Home page
+- 2 product pages with data managed with json server
+- Sorting and Filtering functionality
+
+
+
+## Run Locally
+
+Pre-Requisite : 
+VS Code
+
+Clone the project
+
+```bash
+#   
+```
+
+
+## Website Flow
+
+- Signup and Login pages 
+![](login&signup.jpg)
+
+- Homepage, Men & Women 
+![](homepage.jpg)
+
+- Product pages 
+![](products.jpg)
+
+- Footer
+![](footer.jpg)
+
+
+
+
+## Author
+
+- [Amjad] (https://github.com/AmjadHusain-1)
---
+++ men.js
@@ -0,0 +1,136 @@
+
+
+
+let productDiv =document.getElementById("product")
+
+
+// function for display the product form data json and give divin html file
+function displayProduct(data){
+    data.forEach((obj) => {
+        let div=document.createElement("div");
+        
+        let img=document.createElement("img");
+        img.src=obj.Image;
+        img.style.height="200px"
+
+        let comName=document.createElement("h5");
+        // comName.innerText=obj.company;
+
+        let prodName=document.createElement("p");
+        prodName.innerText=obj.Product;
+
+        let rate=document.createElement("p");
+        rate.innerText=`★★★☆ ${obj.Rating}`;
+
+        let price=document.createElement("p");
+        price.innerText=`$ ${obj.Price}`
+
+
+        div.append(img,comName,prodName,rate,price)
+        productDiv.append(div)
+    });
+}
+
+fetch("./men.json")
+    .then((res)=>res.json())
+    .then((data)=>displayProduct(data))
+    .catch((err)=>console.log(err))
+
+
+    function sortPrice(){
+
+        let sortBy =document.getElementById("chooseRate").value
+    
+        
+        let a=sortBy.toString()
+    
+        if(a==1){
+            
+            sortPriceLowToHigh()
+    
+        }else if(a==2){
+            
+            sortPriceHighToLow()
+        }
+    
+        
+    }
+    
+        
+        function sortPriceLowToHigh(){
+            
+            // sorting data according to price
+            fetch("./men.json")
+            .then((res)=>res.json())
+            .then((data)=>{
+                data.sort(function(a,b){
+                    return a.Price - b.Price
+                })
+                // privius data will null
+                productDiv.innerHTML=null
+                displayProduct(data)
+            })
+            .catch((err)=>console.log(err))
+        
+        }
+    
+    
+        function sortPriceHighToLow(){
+            
+            // sorting data according to price
+            fetch("./men.json")
+            .then((res)=>res.json())
+            .then((data)=>{
+                data.sort(function(a,b){
+                    return b.Price - a.Price
+                })
+                // privius data will null
+                productDiv.innerHTML=null
+                displayProduct(data)
+            })
+            .catch((err)=>console.log(err))
+        
+
+}
+// next filter//
+
+function sortRating(){
+
+    let sortBy =document.getElementById("chooseRating").value
+    let a=sortBy.toString()
+    
+    if(a==1){
+            
+        sortRatingLowToHigh()
+    
+    }else if(a==2){
+            
+        sortRatingHighToLow()
+    }
+}
+    
+    // sort low to high
+function sortRatingLowToHigh(){
+            
+    // sorting data according to Rating
+    mainData.sort(function(a,b){
+        return a.Rating - b.Rating
+    })
+    // privius data will null
+    productDiv.innerHTML=null
+    displayProduct(mainData)
+        
+}
+    
+// sort high to low
+function sortRatingHighToLow(){
+            
+    // sorting data according to Rating
+    mainData.sort(function(a,b){
+        return b.Rating - a.Rating
+    })
+    // privius data will null
+    productDiv.innerHTML=null
+    displayProduct(mainData)
+        
+}
